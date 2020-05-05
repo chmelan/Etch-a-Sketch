f2c07f513503c94440c3b7c765ec10aae7a90ec5
chmelan <chmelan@comcast.net>
2020-05-05 10:00:04 -0400

    added basic drawing feature

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,7 @@
 
 const container = document.querySelector("#container");
 
-function makeRows(length) {
+function makeGrid(length) {
   container.style.setProperty('--grid-rows', length);
   container.style.setProperty('--grid-cols', length);
   for (i = 0; i < (length * length); i++) {
@@ -10,4 +10,14 @@ function makeRows(length) {
   };
 };
 
-makeRows(16);
+
+
+makeGrid(16);
+
+const divContainer = container.querySelectorAll("div");
+divContainer.forEach(div=>{;
+    div.addEventListener("mouseover", (e) =>{;
+        div.classList.add("black");
+        e.stopPropagation(); //stops propegation
+    });
+});
