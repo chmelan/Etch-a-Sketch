10475ab508bbd586d256620b8d100069d912a55b
chmelan <chmelan@comcast.net>
2020-05-06 12:25:15 -0400

    added clear button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,7 +1,19 @@
 
 const container = document.querySelector("#container");
 
-function makeGrid(length) {
+const blackBtn = document.querySelector("#blackBtn");
+const greyScaleBtn = document.querySelector("#greyScaleBtn");
+const rainbowBtn = document.querySelector("#rainbowBtn");
+const warmBtn = document.querySelector("#warmBtn");
+const coolBtn = document.querySelector("#coolBtn");
+const clearBtn = document.querySelector("#clearBtn");
+const sizeBtn = document.querySelector("#sizeBtn");//complete
+let drawColor = "black"
+
+
+
+
+function makeGrid(length) { //makes a grid with a width and height of "length"
   container.style.setProperty('--grid-rows', length);
   container.style.setProperty('--grid-cols', length);
   for (i = 0; i < (length * length); i++) {
@@ -14,20 +26,20 @@ function promptGridSize(){
     return parseInt(prompt("Enter the value for the size of the grid" , 16));
 }
 
-function changeGridSize(){ //allows the user to change the griz size
-const sizeButton = document.querySelector("#sizeBtn")
-sizeButton.addEventListener("click", (e)=>{
+function changeGridSize(){ //allows the user to change the grid size
+sizeBtn.addEventListener("click", (e)=>{
     removeAllChildren(container); // removes old divs
     makeGrid(promptGridSize()); // creates a new grid size with a user prompt
-    enableChangeOnMouseover(); //allows for drawing on newly created divs
+    enableDrawOnMouseover(drawColor); //allows for drawing on newly created divs
     });
 }
 
-function enableChangeOnMouseover(){
+function enableDrawOnMouseover(drawColor){
 const divContainer = container.querySelectorAll("div");
 divContainer.forEach(div=>{;
     div.addEventListener("mouseover", (e) =>{;
-        div.classList.add("black");
+        div.className = "";
+        div.classList.add(drawColor);
         e.stopPropagation(); //stops propegation upwards
     });
 });
@@ -37,14 +49,22 @@ function removeAllChildren(parentNode){
         parentNode.removeChild(parentNode.lastChild);
     }
 }
+
 function resetCellClass(){
     const divContainer = container.querySelectorAll("div");
     divContainer.forEach(div=>{
-        div.className = "grid-item"
+        //div.className = "";
+        div.className = "grid-item";
     })
     
 }
+function enableClearButton(){
+    clearBtn.addEventListener("click", (e) => {
+        resetCellClass();
+    });
+};
 
 makeGrid(16);
-enableChangeOnMouseover();
+enableDrawOnMouseover(drawColor);
 changeGridSize();
+enableClearButton();
